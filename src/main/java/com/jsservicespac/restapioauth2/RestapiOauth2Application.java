2bd4e376fe798e67ba44ac075f473c8fe2c1e33a
john_stuart <lubwamajohnstuart201@gmail.com>
2025-04-16 15:11:41 +0300

    feat:initial commit

---
+++ src/main/java/com/jsservicespac/restapioauth2/RestapiOauth2Application.java
@@ -0,0 +1,13 @@
+package com.jsservicespac.restapioauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsservicespac/restapioauth2/RestapiOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsservicespac.restapioauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

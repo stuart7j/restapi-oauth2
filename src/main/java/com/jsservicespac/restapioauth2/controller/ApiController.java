c092e7daf3a1683f1092a356f36fe18cd6d884a4
john_stuart <lubwamajohnstuart201@gmail.com>
2025-04-17 14:10:23 +0300

    feat:Add readme

--- src/main/java/com/jsservicespac/restapioauth2/controller/ApiController.java
+++ src/main/java/com/jsservicespac/restapioauth2/controller/ApiController.java
@@ -1,6 +1,5 @@
 package com.jsservicespac.restapioauth2.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
